d1f8fff9e74af3c3c29c9a2e20ab9f790560e264
AleEirea97 <ale_eirea@hotmail.com>
2025-06-18 20:14:18 -0300

    Segunda consigna del TP2

--- 3_trabajos_practicos/tp2/firmware/firmware.c
+++ 3_trabajos_practicos/tp2/firmware/firmware.c
@@ -1,11 +1,15 @@
 #include <stdio.h>
 #include "pico/stdlib.h"
 #include "hardware/adc.h"
+#include "hardware/irq.h"   //Se añade esto para interrupciones
 
 #include "FreeRTOS.h"
 #include "task.h"
 #include "queue.h"
 
+
+// Cantidad de muestras del adc
+#define SAMPLES 4
 
 /*Estructura para pasar los datos del sensor*/
 typedef struct {
@@ -17,37 +21,54 @@ typedef struct {
 // Cola para datos del sensor
 QueueHandle_t queue_sensor;
 
+//Handler para la interrupcion del ADC
+
+void adc_irq_handler(void) {
+    // Variable para verificar la necesidad de un cambio tarea
+    BaseType_t to_higher_priority_task = false;
+    // Deshabilito la interrupcion y detengo el ADC
+    adc_irq_set_enabled(false);
+    adc_run(false);
+    // Variable para calcular el promedio de muestras
+    uint32_t raw = 0;
+    for(uint8_t i = 0; i < SAMPLES; i++) { raw += adc_fifo_get(); }
+    // Limpio el FIFO
+    adc_fifo_drain();
+    // Datos para la cola
+    sensor_data_t data = { .raw = (uint16_t) (raw / SAMPLES) };
+    data.voltage = data.raw * 3.3 / (1 << 12);
+    // El calculo de temperatura sale de la documentacion del SDK
+    data.temperature = 27 - (data.voltage - 0.706) / 0.001721;
+    // Envio por cola
+    xQueueOverwriteFromISR(queue_sensor, &data, &to_higher_priority_task);
+    // Reviso si es necesario el cambio a otra tarea
+    portYIELD_FROM_ISR(to_higher_priority_task);
+}
+
+
+
 /*Tarea de inicializacion*/
-void task_init(void *params) {
+void task_init(void *params) { 
     // Inicializacion del ADC y sensor de temperatura
     adc_init();
     adc_set_temp_sensor_enabled(true);
     adc_select_input(ADC_TEMPERATURE_CHANNEL_NUM);
     // Inicializacion de cola para estructura
     queue_sensor = xQueueCreate(1, sizeof(sensor_data_t));
+
+    // Inicializo la interrupcion del ADC y la cantidad de lecturas necesarias
+    adc_fifo_setup(true, false, SAMPLES, false, false);
+    adc_irq_set_enabled(true);
+    irq_set_exclusive_handler(ADC_IRQ_FIFO, adc_irq_handler);
+    irq_set_enabled(ADC_IRQ_FIFO, true);
+    adc_run(true);
+
+
     // Elimino tarea para liberar recursos
     vTaskDelete(NULL);
 }
 
-
-/* Tarea que lee el ADC */
-void task_adc(void *params) {
-    // Estructura para la cola
-    sensor_data_t data = {0};
-
-    while(1) {
-        // Leo el sensor y preparo los datos para la cola
-        data.raw = adc_read();
-        data.voltage = data.raw * 3.3 / (1 << 12);
-        // El calculo de temperatura sale de la documentacion del SDK
-        data.temperature = 27 - (data.voltage - 0.706) / 0.001721;
-        // Escribo la cola siempre con el ultimo valor
-        xQueueOverwrite(queue_sensor, &data);
-    }
-}
-
-
-/* Tarea que escribe por consola */
+//Tarea que escribe por consola     Se añade.
 void task_print(void *params) {
     // Estructura para la cola
     sensor_data_t data = {0};
@@ -64,6 +85,18 @@ void task_print(void *params) {
     }
 }
 
+
+/* Tarea que lee el ADC */
+void task_adc(void *params) {
+
+    while(1) {
+        // Habilito la interrupcion del ADC nuevamente y corro las conversiones
+        adc_irq_set_enabled(true);
+        adc_run(true);
+        // Bloqueo la tarea por un tiempo
+        vTaskDelay(pdMS_TO_TICKS(500));
+    }
+}
 
 /* Programa principal */
 int main(void) {
