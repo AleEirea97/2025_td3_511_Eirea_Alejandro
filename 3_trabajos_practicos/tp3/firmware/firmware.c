e51d3f80d5de4ba0974da9e768fcf7fe1d5b2b7d
AleEirea97 <ale_eirea@hotmail.com>
2025-06-25 20:41:51 -0300

    Segunda Consigna del TP3

--- 3_trabajos_practicos/tp3/firmware/firmware.c
+++ 3_trabajos_practicos/tp3/firmware/firmware.c
@@ -1,73 +1,99 @@
 #include <stdio.h>
 #include "pico/stdlib.h"
+#include "hardware/i2c.h"
 
 #include "FreeRTOS.h"
 #include "task.h"
 #include "semphr.h"
 #include "queue.h"
 
-#include "helper.h"   //Para generar el PWM
+#include "helper.h" //Biblioteca para generar pulsos y testear.
+
+#include "lcd.h"
+
+#define I2C_PORT i2c0
+#define I2C_SDA  8   //PIN 11-GPIO8
+#define I2C_SCL  9   //PIN 12-GPIO9
+#define ADDR     0x27    // Direccion de 7 bits del adaptador del LCD
 
 TaskHandle_t handle_Task_FREC = NULL;
 
-SemaphoreHandle_t semphrCounting;   // Semaforo para CONTAR LOS FLANCOS
+SemaphoreHandle_t semphrCounting;   // Semaforo para CONTAR LOS FLANCOS.
 
-#define PIN_ENT_SEÑAL 18     //GPIO 24 COMO ENTRADA
-#define PIN_GEN_SEÑAL 19     //GPIO 25 COMO SALIDA PWM
-#define FREC_PWM 9500        //Frecuencia para el PWM
+#define PIN_ENT_SEÑAL 18     //GPIO 18 COMO ENTRADA DE SEÑAL: PIN 24
+#define PIN_GEN_SEÑAL 19     //GPIO 19 COMO SALIDA DE PWM: PIN 25
+#define FREC_PWM      9500     //Frecuencia para el Helper
+
+void gpio_callback(uint gpio, uint32_t events) {
+    BaseType_t xHigherPriorityTaskWoken = pdFALSE;
+
+    if (gpio == PIN_ENT_SEÑAL && (events & GPIO_IRQ_EDGE_RISE)) {
+        xSemaphoreGiveFromISR(semphrCounting, &xHigherPriorityTaskWoken);
+        portYIELD_FROM_ISR(xHigherPriorityTaskWoken);
+    }
+}
 
 //TAREA FRECUENCIMETRO, DETECTOR DE FLANCOS ASCENDENTES
 void task_frec(void *params) {
     
-    bool estado_anterior = false;
-    bool estado_actual = false;
-    uint16_t flancos_ascendentes = 0;
     TickType_t tiempo_ms = xTaskGetTickCount();
     int contador = 0;
+    char str1[20]="FREC:";
+    char str2[20]="";
     
-    while(1) {
-        
-        estado_anterior = estado_actual;
-        estado_actual = gpio_get(PIN_ENT_SEÑAL);
+    //HABILITO LA INTERRUPCION DEL PIN 24 POR FLANCOS ASCENDENTES
+    gpio_set_irq_enabled_with_callback(PIN_ENT_SEÑAL, GPIO_IRQ_EDGE_RISE, true, &gpio_callback);
 
-        if(estado_actual && !estado_anterior){
-            xSemaphoreGive(semphrCounting);  //flancos_ascendentes ++;
-        }
+    while(1){
 
         if(xTaskGetTickCount() - tiempo_ms > 1000){
             xSemaphoreTake( semphrCounting, 0 );
             contador = uxSemaphoreGetCount(semphrCounting);
-            printf("Frecuencia: %i Hz\n", contador);
-            printf("Error de conteo: %d Hz\n", FREC_PWM-contador);
-            tiempo_ms = xTaskGetTickCount();
-            xQueueReset(semphrCounting);    //flancos_ascendentes = 0;
+            lcd_clear();
+            lcd_set_cursor(0,0);
+            lcd_string(str1);
+            lcd_set_cursor(0, 6);
+            sprintf(str2, "%i Hz", contador);
+             lcd_string(str2);
+            //lcd_set_cursor(1,0);
+            //sprintf("Error:", "%i Hz", FREC_PWM-contador);
 
+            tiempo_ms = xTaskGetTickCount();
+            xQueueReset(semphrCounting);        //flancos_ascendentes = 0;
         }
+    vTaskDelay(pdMS_TO_TICKS(10));  // Pequeño delay para no saturar la CPU
 
     }
 }
 
-/**
- * @brief Programa principal
- */
-int main(void) {
+
+int main()
+{
     stdio_init_all();
 
     // Inicializacion de GPIO ENTRADA SEÑAL
     gpio_init(PIN_ENT_SEÑAL);
-    gpio_set_dir(PIN_ENT_SEÑAL, false); //true para salida, false para entrada
+    gpio_set_dir(PIN_ENT_SEÑAL, false);         //true para salida, false para entrada
     gpio_pull_down(PIN_ENT_SEÑAL);
 
+    i2c_init(I2C_PORT, 400*1000);                    // Inicializo el I2C con un clock de 400 KHz
+    gpio_set_function(I2C_SDA, GPIO_FUNC_I2C);  // Habilito la funcion de I2C en los GPIOs
+    gpio_set_function(I2C_SCL, GPIO_FUNC_I2C);
+    gpio_pull_up(I2C_SDA);       // Habilito pull-ups
+    gpio_pull_up(I2C_SCL);
+    lcd_init(I2C_PORT, ADDR);         // Inicializo LCD
+    lcd_clear();                // Limpio el LCD
+
     // Generador de PWM para comprobar contador de flancos
-    pwm_user_init(PIN_GEN_SEÑAL, FREC_PWM);     //Esto es lo que hace el helper.
-    
+    pwm_user_init(PIN_GEN_SEÑAL, FREC_PWM);     //Esto es lo que se hace con Helper
+
     //Creacion de Semaforo Counting
-    semphrCounting = xSemaphoreCreateCounting( 10000, 0 );  
+    semphrCounting = xSemaphoreCreateCounting( 10000, 0 );  //Limite de conteo: 10k. Inicio de conteo: 0.
     
     // Creacion de tareas
     xTaskCreate(task_frec, "Task_Frecuencimetro", 4*configMINIMAL_STACK_SIZE, NULL, 2, NULL);
 
     // Arranca el scheduler
     vTaskStartScheduler();
-    while(1);
+    while (true);
 }
